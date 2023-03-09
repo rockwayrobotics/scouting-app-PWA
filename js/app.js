3b81b38664d8257f4d72cd06ec5aa42a7a3910b1
Erin Abicht <contact@the-system.eu.org>
2023-03-09 16:19:50 -0500

    Persistent data

--- js/app.js
+++ js/app.js
@@ -27,11 +27,19 @@ request.onupgradeneeded = (event) => {
 	};
 };
 
+var Match = {
+	team: 0,
+	load: function() {
+		return Match.team;
+	}
+}
+
 // Components
 var NavBar = {
 	view: function() {
 		return m("nav",
 			m("ul",
+				m("li", m("a", { class: "button", href: "#!/scout" }, "Home" )),
 				m("li", m("a", { class: "button", href: "#!/scout/pit" }, "Pit Scout" )),
 				m("li", m("a", { class: "button", href: "#!/scout/match" }, "Match Scout" )),
 				m("li", m("a", { class: "button", href: "#!/driver" }, "Driver Meeting" ))
@@ -51,11 +59,45 @@ var ScoutSetup = {
 	view: function() {
 		return m("div", { class: "main" },
 			m(NavBar),
-			m("form", [
+			m("form", {
+				onsubmit: function(e) {
+					e.preventDefault()
+					window.location.href = "#!/scout/pit";
+				}
+			}, [
 				m("label.label", "Team #"),
-				m("input.input[type=number][placeholder=8089]"),
+				m("input.input[type=number][placeholder=8089]", {
+					oninput: function(e) {
+						Match.team = e.target.value;
+						console.log("Team " + Match.team);
+					}
+				}),
 				m("button.button[type=submit]", "Continue"),
 			])
+		)
+	}
+}
+
+var ScoutMatch = {
+	view: function() {
+		return m("div", { class: "main" }, [
+			m(NavBar),
+			m("div", [
+				m("h1", "Match Scouting"),
+				m("h2", "Team #"+Match.team),
+			]),
+		])
+	}
+}
+
+var ScoutPit = {
+	view: function() {
+		return m("div", { class: "main" },
+			m(NavBar),
+			m("div", [
+				m("h1", "Pit Scouting"),
+				m("h2", "Team #"+Match.team),
+			]),
 		)
 	}
 }
@@ -63,7 +105,7 @@ var ScoutSetup = {
 m.route(root, "/splash", {
     "/splash": Splash,
 	"/scout": ScoutSetup,
-	// "/scout/pit": ScoutPit,
-	// "/scout/match": ScoutMatch,
+	"/scout/pit": ScoutPit,
+	"/scout/match": ScoutMatch,
 	// "/driver": DriverMeeting,
 })
